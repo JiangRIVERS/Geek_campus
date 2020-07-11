b211b662c75a09311a2bba1a5bad43d7bf4f7251
JiangRIVERS <jiangrivers9511@gmail.com>
2020-07-11 23:47:44 +0800

    fighting

--- teskbank/leetcode/editor/cn/[105]从前序与中序遍历序列构造二叉树.py
+++ teskbank/leetcode/editor/cn/[105]从前序与中序遍历序列构造二叉树.py
@@ -1,12 +1,12 @@
-# 根据一棵树的中序遍历与后序遍历构造二叉树。 
+# 根据一棵树的前序遍历与中序遍历构造二叉树。 
 # 
 #  注意: 
 # 你可以假设树中没有重复的元素。 
 # 
 #  例如，给出 
 # 
-#  中序遍历 inorder = [9,3,15,20,7]
-# 后序遍历 postorder = [9,15,7,20,3]
+#  前序遍历 preorder = [3,9,20,15,7]
+# 中序遍历 inorder = [9,3,15,20,7] 
 # 
 #  返回如下的二叉树： 
 # 
@@ -14,8 +14,7 @@
 #    / \
 #   9  20
 #     /  \
-#    15   7
-#  
+#    15   7 
 #  Related Topics 树 深度优先搜索 数组
 
 
@@ -28,15 +27,17 @@
 #         self.right = None
 
 class Solution:
-    def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        dict_ = {value:idx for idx,value in enumerate(inorder)}
         def helper(left, right):
             if left > right:
                 return
-            root = TreeNode(postorder.pop())
-            rootidx = dict_[root.val]
-            root.right = helper(rootidx + 1, right)
-            root.left = helper(left, rootidx - 1)
+            rootvalue = preorder.pop(0)
+            root = TreeNode(rootvalue)
+            index = dict_[rootvalue]
+            root.left = helper(left, index - 1)
+            root.right = helper(index + 1, right)
             return root
-        dict_ = {rootval:idx for idx,rootval in enumerate(inorder)}
         return helper(0, len(inorder) - 1)
+
 # leetcode submit region end(Prohibit modification and deletion)

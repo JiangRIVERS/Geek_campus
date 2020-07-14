3dee143b6915c3ca214ba9a1b677044357a1aff1
JiangRIVERS <jiangrivers9511@gmail.com>
2020-07-14 23:20:41 +0800

    fighting

---
+++ teskbank/leetcode/editor/cn/[17]电话号码的字母组合.py
@@ -0,0 +1,35 @@
+# 给定一个仅包含数字 2-9 的字符串，返回所有它能表示的字母组合。 
+# 
+#  给出数字到字母的映射如下（与电话按键相同）。注意 1 不对应任何字母。 
+# 
+#  
+# 
+#  示例: 
+# 
+#  输入："23"
+# 输出：["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+#  
+# 
+#  说明: 
+# 尽管上面的答案是按字典序排列的，但是你可以任意选择答案输出的顺序。 
+#  Related Topics 字符串 回溯算法
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        hashmap = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+                   '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+        res = []
+        if not digits:
+            return res
+        def helper(current_digits, s):
+            if len(s) == len(digits):
+                res.append(s)
+                return
+            values = hashmap[current_digits[0]]
+            for i in values:
+                helper(current_digits[1:], s + i)
+        helper(digits, '')
+        return res
+# leetcode submit region end(Prohibit modification and deletion)
